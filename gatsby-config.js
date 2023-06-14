dd4565d5d6b5c7c60104afa41921c028ab1a288f
Marc Blickle <marcblickle@gmx.de>
2023-06-15 00:23:33 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `starcsiteU`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "FuonmDLGT5qeBXvBIrTZPZzy4IR3A94XTJxYv2O-9jQ",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
+};
